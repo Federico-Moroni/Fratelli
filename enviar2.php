324ef6a2ec7c5ac1a84cbfc63139f1aaae68148e
Federico Moroni <fede.moroni1993@gmail.com>
2021-09-04 17:50:18 -0300

    Faltan imagenes PNG

--- enviar2.php
+++ enviar2.php
@@ -6,22 +6,22 @@ $phone = $_POST ['phone']
 $message = $_POST ['message']
 
 // Como funciona con el server
-$header = 'From: ' . $mail . " \r\n";
+$header = 'From: ' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-// Como me a llegar el cuerpo del mail a mi, o sea lo que la gente escribió en el form
-$mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
-$mensaje .= "Su e-mail es: " . $email . " \r\n";
-$mensaje .= "Su telefono es: " . $phone . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+// Como me va llegar el cuerpo del mail a mi, o sea lo que la gente escribió en el form
+$message = "Este mensaje fue enviado por " . $name . ",\r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Su telefono es: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$message .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'fede.moroni1993@gmail.com'; // El mail a donde van a llegar los mensajes
 $asunto = 'Mensaje Fratelli';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($message), $header);
 
 header('Location:index.html');
 
